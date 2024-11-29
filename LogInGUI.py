59512057e22728593f6f0997a9ef9413ed2c9fac
RMcCauley03 <mccaulr2@my.erau.edu>
2024-11-28 23:00:36 -0500

    Add files via upload
    made a login gui that stores information encrypted

---
+++ LogInGUI.py
@@ -0,0 +1,110 @@
+import csv
+from tkinter import *
+import os
+import string
+
+root = Tk()
+
+root.title("Login Page")
+root.geometry('1920x1080')
+
+def login_page(): # Sets up GUI for login page
+    username_label = Label(root, text = "Enter Username:")
+    username_label.grid(column = 0, row = 0)
+
+    username_entered = Entry(root, width=10)
+    username_entered.grid(column = 1, row = 0)
+
+    password_label = Label(root, text = "Enter Password:")
+    password_label.grid(column = 0, row = 1)
+
+    password_entered = Entry(root, width = 10, show = "*")
+    password_entered.grid(column = 1, row = 1)
+
+    login_button = Button(root, text = "Enter", fg = "red", command = lambda: button_click(username_entered.get(), password_entered.get()))
+    login_button.grid(column = 2, row=0)
+
+
+def button_click(username: str, password: str):
+    if check_login(username, password):
+        success_label = Label(root, text = "Username & Password Accepted", fg = "green")
+        success_label.grid(column = 1, row = 3)
+    else:
+        failure_label = Label(root, text = "Username & Password Denied", fg = "red")
+        failure_label.grid(column = 1, row = 3)
+
+
+def check_login(username: str, password: str):
+    encrypted_login_info = load_login_info()
+    decrypted_login_info = decrypt_login_info(encrypted_login_info)
+    print(encrypted_login_info)
+    print(decrypted_login_info)
+
+    for row in decrypted_login_info:
+        if username == row[0] and password == row[1]:
+            return True
+    
+    return False
+
+
+def load_login_info(): # Makes an array for all of the stored usernames and passwords
+    script_dir = os.path.dirname(os.path.abspath(__file__)) # Ensures the file is in the same directory as the script (GPT)
+    os.chdir(script_dir)
+
+    encrypted_login_info = []
+    try:
+        with open('login_info.csv', 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                username = row[0]
+                password = row[1]
+                type = row[2]
+                encrypted_login_info.append([username, password, type])
+    except FileNotFoundError:
+        print("Error: The file was not found.")
+    
+    return encrypted_login_info
+
+
+def decrypt_login_info(encrypted_login_info: list):
+    character_list = make_character_list()
+    encryption_key = make_encryption()
+
+    decrypted_login_info = []
+
+    for row in encrypted_login_info:
+        encrypted_username = row[0]
+        encrypted_password = row[1]
+        type = row[2]
+
+        decrypted_username = []
+        decrypted_password = []
+
+        for letter in encrypted_username:
+            position = encryption_key.index(letter)
+            decrypted_username.append(character_list[position])
+        for letter in encrypted_password:
+            position = encryption_key.index(letter)
+            decrypted_password.append(character_list[position])
+
+        decrytped_username_string = ''.join(decrypted_username)
+        decrytped_password_string = ''.join(decrypted_password)
+
+        decrypted_login_info.append([decrytped_username_string, decrytped_password_string, type])
+    
+    return decrypted_login_info
+    
+
+def make_character_list():
+    all_characters = " " + string.ascii_letters + string.digits + string.punctuation
+    all_characters = list(all_characters)
+    return all_characters
+    
+
+def make_encryption():
+    encryption_key = ['&','R','~','N','e','C','B','_','Z','d',':','O','q','u','|','h','!','3','V','X','4','[','w','s','U','"','0','L','J','$','?','k','j','a','z','b','c',')','9','-','r','I','H','y','P','+','D','#','/','E','Q','A','}','o','.','W','1','(','p','5',';','^','T','\\','i',"'",'x','<','6','K','@','{',',','7',' ','F','Y','t','G','v','=','`','*','2',']','f','n','m','S','>','g','%','M','8','1']
+    return encryption_key # Terribly hardcoded for now because if randomized every iteration, code wont work
+
+
+login_page()
+root.mainloop()
